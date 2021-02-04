1019d92861c8d1402f6dea68d8600e9576d0164e
age174 <76593644+age174@users.noreply.github.com>
2021-02-04 17:50:29 +0800

    Update dkdsp.js

--- dkdsp.js
+++ dkdsp.js
@@ -21,7 +21,7 @@ TG电报群: https://t.me/hahaha8028
 10 * * * * https://raw.githubusercontent.com/age174/-/main/dkd.js, tag=多看点, img-url=https://raw.githubusercontent.com/shoujiqiyuan/PokemonGOforQuanX/master/IconSet/X003.png, enabled=true
 
 #多看点视频
-5 * * * * https://raw.githubusercontent.com/age174/-/main/dkdsp.js, tag=多看点视频, img-url=https://raw.githubusercontent.com/shoujiqiyuan/PokemonGOforQuanX/master/IconSet/X003.png, enabled=true
+5 9,16,21 * * * https://raw.githubusercontent.com/age174/-/main/dkdsp.js, tag=多看点视频, img-url=https://raw.githubusercontent.com/shoujiqiyuan/PokemonGOforQuanX/master/IconSet/X003.png, enabled=true
 
 [rewrite_local]
 
