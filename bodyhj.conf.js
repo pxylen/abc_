0d8d537ee860323fd8bc992047a57d5cf5620316
age174 <76593644+age174@users.noreply.github.com>
2021-02-07 14:09:29 +0800

    Add files via upload

---
+++ bodyhj.conf.js
@@ -0,0 +1,8 @@
+hostname = api.sxsjyzm.com,ymz.iphonezhuan.com
+
+//羊毛赚
+^http://ymz.iphonezhuan.com/addaction url script-request-body https://raw.githubusercontent.com/age174/-/main/ymz.js
+
+//芝嫲视频
+
+^https://api.sxsjyzm.com/api2/loot/index url script-request-body https://raw.githubusercontent.com/age174/-/main/zmsp.js
