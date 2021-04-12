a1aafbe778be27348a8eb909c8a24c5349dea4a2
age174 <76593644+age174@users.noreply.github.com>
2021-04-12 16:51:58 +0800

    Update ximei.js

--- ximei.js
+++ ximei.js
@@ -136,7 +136,7 @@ name = result.data[0].data.translatedTitle
         console.log(`\n西梅获取文章列表成功\n文章ID:${id}\n文章标题:${name}\n执行点赞任务`)
 
 
-await ximeidz();      
+await ximeipl();      
         
 } else {
 console.log('西梅获取用户信息失败 已停止当前账号运行!')
@@ -240,11 +240,11 @@ let url = {
 
         console.log('\n西梅阅读成功,获得梅子:'+result.data.point)
         await $.wait(1000);
-        await ximeisp();
+        await ximei1();
 } else {
        console.log('\n西梅阅读失败  '+result.msg)
 await $.wait(1000);
-        await ximeisp();
+        
 }
    
         } catch (e) {
@@ -273,11 +273,12 @@ let url = {
 
         console.log('\n西梅视频成功,获得梅子:'+result.data.point)
         await $.wait(1000);
-        await ximeifx();
+        //await ximeifx();
+        
 } else {
        console.log('\n西梅视频失败  '+result.msg)
 await $.wait(1000);
-        await ximeifx();
+        //await ximeifx();
 }
    
         } catch (e) {
@@ -307,7 +308,7 @@ let url = {
         console.log('\n西梅分享成功,获得梅子:'+result.data.point)
 
         await $.wait(10000);
-        await ximei1();
+        //await ximei1();
 } else {
        console.log('\n西梅分享失败  '+result.msg)
 
