496d75d278d395e6dc1533a4b4bae33007e1eae9
lensu <1217079729@qq.com>
2021-06-15 14:33:50 +0800

    update

--- scripts/dingdong_yangyu2.js
+++ scripts/dingdong_yangyu2.js
@@ -242,7 +242,7 @@ function fishpond() {
             }
             const data = response.data;
             const cray = FreshCray(data);
-            const pet = cray ? cray : data.seeds[0];
+            const pet = cray ? cray : data.baseSeed;
             const petName = cray ? "小龙虾" : "鱼";
             if(pet.expPercent >= 100){
                 $.alert(`去看看,${petName}应该已经养活了`, "userguide/detail");
@@ -251,8 +251,8 @@ function fishpond() {
                 }
                 // 龙虾完事继续养鱼
             }
-            propsId = data.props[0].propsId;
-            const amount = data.props[0].amount;
+            propsId = data.feed.propsId;
+            const amount = data.feed.amount;
             $.log(`当前饲料剩余:${amount}g,${pet.msg}`);
             if(amount < 10){
                 $.alert("饲料不够，下次再喂吧。");
